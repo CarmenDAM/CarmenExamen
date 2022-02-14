fad556a8888e2b6654dabf9b1c63c75232f150ee
CarmenDAM <cceballose01@educantabria.es>
2022-02-14 13:20:47 +0100

    1 file modified

--- src/data/App.js
+++ src/data/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Menu from './components/Menu';
-import { PaginasApp } from './data/PaginasApp';
+import { MenuItems } from './data/MenuItems';
 
 export default function App() {
   return (
     <Router>
       <Menu />
-      {PaginasApp.map((item) => {
+      {MenuItems.map((item) => {
         return (
           <Route
             key={item.id}
